d3a2682785f35e520d427b94d41d6f9decc0f515
Ahmed Mansour <ahmedmansour944@gmail.com>
2024-05-01 11:12:09 +0300

    first commit

---
+++ portfolio/src/App.js
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    
+      <>
+        <h1 className="title"> Ahmed Mansour</h1>
+        <h3 className="title"> Esraa Mohamed</h3>
+      </>
+    
+  );
+}
+
+export default App;
